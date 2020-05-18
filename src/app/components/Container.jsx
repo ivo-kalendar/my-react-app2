8f104c32ea90c56e2354cf06c9375fdb743565ca
Ivo <ivokalendar@icloud.com>
2020-05-18 21:01:50 +0200

    Playing with useState...

--- src/app/components/Container.jsx
+++ src/app/components/Container.jsx
@@ -6,10 +6,16 @@ const { users, comments } = store.getState()
 export const Container = () => {
 	const [ resourceType, setResourceType ] = useState('something...')
 	const [ secondType, setSecondType ] = useState('Ништо не е Притиснато...')
-	const [count, setCount] = useState(0)
+	const [ count, setCount ] = useState(0)
+	const [ width, setWidth ] = useState(window.innerWidth)
+	const [ height, setHeight ] = useState(window.innerHeight)
 
 	let decrementCount = () => setCount(prevCount => (prevCount !== 0) ? (prevCount - 1) : 0)
 	let incrementCount = () => setCount(prevCount => (prevCount !== 10) ? (prevCount + 1) : 10)
+	window.onresize = () => {
+		setWidth(window.innerWidth)
+		setHeight(window.innerHeight)
+	}
 
 	return <>
 		<div className="tester">
@@ -21,6 +27,11 @@ export const Container = () => {
 
 
 		<h1>{resourceType}</h1>
+		<h1>{secondType}</h1>
+		<h1>
+			Window Width: <span className="resize"> {width} </span>
+			Window Height: <span className="resize"> {height} </span>
+		</h1>
 		<div id="container">
 			{comments.map(comment => {
 				 return <h2 onClick={() => setResourceType(comment.content)} key={comment.id}>
@@ -39,6 +50,5 @@ export const Container = () => {
 			<Main />
 			<Main />
 		</div>
-		<h1>{secondType}</h1>
 	</>
 }
--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,11 @@ module.exports = {
 		publicPath: '/'
 	},
 	resolve: {extensions: ['.js', '.jsx']},
-	devServer: {host: '192.168.0.100',historyApiFallback: true},
+	devServer: { 
+		host: '192.168.0.100', 
+		port: '7788',
+		historyApiFallback: true 
+	},
 	module: {rules: [
 		{ test: /\.jsx?/, loader: 'babel-loader' },
 		{ 
